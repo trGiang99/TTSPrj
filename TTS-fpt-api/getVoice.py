1783c6ad595d7d1e4c0da8a46a2907b92a2cc7c9
trGiang99 <37588363+trGiang99@users.noreply.github.com>
2019-05-25 17:17:29 +0700

    fix getVoice.py

--- TTS-fpt-api/getVoice.py
+++ TTS-fpt-api/getVoice.py
@@ -7,29 +7,14 @@ from merge_mp3 import merge_mp3
 removeBOMfromInputFile()
 
 
-# inputs = get_list_input()
-# for item in inputs:   
-#     # Read data from text file
-#     file = open(f'textData/{item}.txt', 'r', encoding='utf-8')
-#     content = file.read()
+inputs = get_list_input()
+for item in inputs:   
+    # Read data from text file
+    file = open(f'textData/{item}.txt', 'r', encoding='utf-8')
+    content = file.read()
 
-    # # Send request to FPT Speech Synthesize
-    # request_to_FSS(content)
+    # Send request to FPT Speech Synthesize
+    request_to_FSS(content)
 
-    # # Merge mp3 file to destination mp3 file
-    # merge_mp3(item)
-
-
-item = env.INPUT_DIR
-# Read data from text file
-file = open(f'textData/{item}.txt', 'r', encoding='utf-8')
-content = file.read()
-
-
-# Send request to FPT Speech Synthesize
-request_to_FSS(content)
-
-
-# Merge mp3 file to destination mp3 file
-merge_mp3(item)
-
+    # Merge mp3 file to destination mp3 file
+    merge_mp3(item)
